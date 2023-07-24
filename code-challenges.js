d2c0213b08507643561215ed192a8bdc0a4450f4
Kiana-Massey <kxm7046@mavs.uta.edu>
2023-07-24 00:30:10 -0700

    completed assessment

--- code-challenges.js
+++ code-challenges.js
@@ -8,35 +8,62 @@
 
 // Pseudo code:
 
-// Set one:
-const fruit1 = "apple"
-const fruit2 = "banana"
-// Expected outcome: "banana"
+// // Set one:
+// const fruit1 = "apple"
+// const fruit2 = "banana"
+// // Expected outcome: "banana"
 
-// Set two:
-const fruit3 = "cherry"
-const fruit4 = "kiwi"
+// // Set two:
+// const fruit3 = "cherry"
+// const fruit4 = "kiwi"
 // Expected outcome: "cherry"
 
 // --------------------1) Create a function that takes in a number and determines if the given number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below.
 
 // Pseudo code:
+// create a variable for boilingPoint that cant be changed, so const.
+// create a function and insert a parameter for a inserted number.
+// create a condition using if/else statements
+// return the num with a string interpolation
+// anything other than a number will return a error message
 
-const temperature1 = 42
-// Expected output: "42 is below boiling point"
+// const temperature1 = 42
+// // Expected output: "42 is below boiling point"
 
-const temperature2 = 350
-// Expected output: "350 is above boiling point"
+// const temperature2 = 350
+// // Expected output: "350 is above boiling point"
 
-const temperature3 = 212
-// Expected output: "212 is at boiling point"
+// const temperature3 = 212
+// // Expected output: "212 is at boiling point"
+
+        const boilingPoint = 212
+        const temp = (num) => {
+            if (num > boilingPoint) {
+                return `${num} is above boiling point` 
+            }
+            else if (num === boilingPoint) {
+                return `${num} is at boiling point`
+            }
+            else if (num < boilingPoint) {
+                return `${num} is below boiling point`
+            }
+            else {
+                return 'error'
+            }
+        }
+        console.log(temp())
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
 // Pseudo code:
+//combine the arrays using concatenation
+//add the length method after concatenation 
+//run console log to execute
 
-const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
-const padres1998WorldSeriesRuns = [6, 3, 5, 3]
+// const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
+// const padres1998WorldSeriesRuns = [6, 3, 5, 3]
+
+        console.log(padres1984WorldSeriesRuns.concat(padres1998WorldSeriesRuns).length)
 // Expected output: 9
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
@@ -44,11 +71,21 @@ const padres1998WorldSeriesRuns = [6, 3, 5, 3]
 // Pseudo code:
 
 const currentCohort = "Echo 2023"
+//create a function
+//use split, reverse, and join built in methods in a return. .split will convert the string into an array, .reverse reverses the order of the values in a array, and .join converts all values from a array into a string.
+//execute the function
+
+        function reverseString(str) {
+            return str.split("").reverse().join("");
+        }
+        console.log(reverseString("Echo 2023"))
+
 // Expected output: "3202 ohcE"
 
 // --------------------4) Create the code that will return the last index of the given value from the array using the test variables provided below.
 
 // Pseudo code:
+//use the .lastindexof method and apply it to the array variable
 
 const numberOfConnections = [13, 34, 42, 5, 5, 10, 27, 42, 10]
 
@@ -58,12 +95,23 @@ const givenValue1 = 42
 const givenValue2 = 10
 // Expected output: 8
 
+        console.log(numberOfConnections.lastIndexOf())
+
 // --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
 
 // Pseudo code:
+//use .sort to modify the array.
+//reverse (a,b) to change it to largest to smallest
 
 const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
+
+        sanDiegoSummerTemperatures.sort(b, a) 
+        console.log(sanDiegoSummerTemperatures)
 // Expected output: [82, 80, 79, 77, 76, 73, 72]
 
+
 const sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
+
+        sanDiegoWinterTemperatures.sort((b, a) => a - b)
+        console.log(sanDiegoWinterTemperatures)
 // Expected output: [68, 67, 66, 66, 62, 59, 59]
--- interview-questions.md
+++ interview-questions.md
@@ -6,54 +6,54 @@ First, without external resources. Challenge yourself to answer from memory as i
 
 Then, research the question to expand on your answer. Even if you feel you have answered the question completely, there is always something more to learn. Write your researched answer in your OWN WORDS.
 
-Type your initials here to confirm you read the instructions:
+Type your initials here to confirm you read the instructions: KM
 
 INSTRUCTOR EXAMPLE: What is a conditional statement?
 
-Your answer:
+Your answer: if/else statements
 
-Researched answer:
+Researched answer: Conditional statements control behavior in JavaScript and determine whether or not pieces of code can run determined by if it is true or false.
 
 1. What is git? What is the difference between git and Github?
 
-Your answer:
+Your answer: Git is the pipeline between your local computer and github to upload files, codes, and projects. Github is the cloud that stores the code, projects, and files.
 
-Researched answer:
+Researched answer: Git is a version control system that lets you manage and keep track of your source code history. GitHub is a cloud-based hosting service that lets you manage Git repositories.
 
 2. Which JavaScript operators will return a Boolean value?
 
-Your answer:
+Your answer: >, <, =, ==, ===
 
-Researched answer:
+Researched answer: NOT (!)(!!), AND (&&), & OR (||) operators
 
-1. What is the difference between an array's index and its value?
+3. What is the difference between an array's index and its value?
 
-Your answer:
+Your answer: what is stated within an array are the values itself. Indexing is outputing the location of the values within the array.
 
-Researched answer:
+Researched answer: Indexing is an operation that pulls out a select set of values from an array. The index of a value in an array is that value's location within the array. 
 
 4. Compare and contrast arrays and strings. Please include ways in which they are the same and ways in which they are different.
 
-Your answer:
+Your answer: Strings are just characters of text. Arrays are collections of different types of data as one.
 
-Researched answer:
+Researched answer: Arrays are used to store collections of data of a particular data type, strings are used to store text values as a sequence of characters.
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-Your answer:
+Your answer: Driver and Navigator. Navigator brainstorms and understands what needs to be done, guides the driver while describing code. The driver is in charge of writing the code that the navigator describes, does the physical work of coding.
 
-Researched answer:
+Researched answer: The driver focuses on the specifics of coding, while the navigator checks the work, code quality and provides direction. 
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Higher Order Functions:
+1. Higher Order Functions: A “higher-order function” is a function that accepts functions as parameters and/or returns a function.
 
-2. Jest:
+2. Jest: Jest is a framework for ‘delightful JavaScript testing.’ It is built on top of Jasmine and is a popular testing framework specifically built to perform mainly unit testing of React and React Native applications.
 
-3. Objects:
+3. Objects: An object is a collection of properties, and a property is an association between a name (or key) and a value.
 
-4. Method:
+4. Method: A set of instructions that are associated with an object.
 
-5. Classes:
+5. Classes: Classes are a template for creating objects. They encapsulate data with code to work on that data. 
